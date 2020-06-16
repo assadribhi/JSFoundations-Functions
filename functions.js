34b8fb1742cc66df6baba4b41b1ff44deef3d38d
assadribhi <assadribhi@gmail.com>
2020-06-16 18:44:16 +0300

    Done!!

--- functions.js
+++ functions.js
@@ -8,7 +8,9 @@
  */
 function greet(name) {
   // Your code here
+  console.log(`Hello ${name}`);
 }
+greet("Assad");
 
 /**
  * isOdd(n):
@@ -21,8 +23,15 @@ function greet(name) {
  */
 function isOdd(n) {
   // Your code here
+  if (n % 2 > 0) {
+    const res = true;
+    return res;
+  } else if (n % 2 < 1) {
+    const res = false;
+    return res;
+  }
 }
-
+isOdd(1);
 /**
  * oddsSmallerThan(n):
  * - receives a number n
@@ -34,8 +43,17 @@ function isOdd(n) {
  */
 function oddsSmallerThan(n) {
   // Your code here
+  if (n % 2 > 0) {
+    let result = n - 1;
+    console.log(result / 2);
+    return result / 2;
+  } else if (n % 2 < 1) {
+    let result = n;
+    console.log(result / 2);
+    return result / 2;
+  }
 }
-
+oddsSmallerThan(16);
 /**
  * squareOrDouble(n):
  * - receives a number n
@@ -48,6 +66,16 @@ function oddsSmallerThan(n) {
  */
 function squareOrDouble(n) {
   // Your code here
+  if (n % 2 > 0) {
+    let result = n * n;
+    console.log(result);
+    return result;
+  } else if (n % 2 < 1) {
+    let result = n * 2;
+    console.log(result);
+    return result;
+  }
 }
+squareOrDouble(9);
 
 module.exports = { greet, isOdd, oddsSmallerThan, squareOrDouble };
